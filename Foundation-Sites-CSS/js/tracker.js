0b3f2b814b5c6f9353bea99dfa99c1282958a7f1
Sherreka Burton <rekadarnb@gmail.com>
2022-04-10 19:15:28 -0400

    made the total investment function

--- Foundation-Sites-CSS/js/tracker.js
+++ Foundation-Sites-CSS/js/tracker.js
@@ -3,17 +3,25 @@
 <td id="coin-number">Number of Coins Goes Here</td>
 <td id="coin-price">Purchase Price Goes Here</td>
 <td id="coin-investment">Total Investment Goes Here</td> */
+// DOM Elements 
 
 let portfolioDisplay = $('#portfolio-row-input');
 let modalEl = $('#exampleModal');
-
+let formEl = $('portfolio-form');
 let coinNameDisplay = $('#coin-name');
+
 
 function printCoinInfo (coinName, date, number, price, investment) {
 
     let portfolioRowEl = $('<tr>');
 
     let coinNameTdEl =  $('<td>').text(coinName);
+
+    fetch(apiUrl)
+    .then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+        });
 
     let coinDateTdEl = $('<td>').text(date);
 
@@ -39,5 +47,11 @@ function printCoinInfo (coinName, date, number, price, investment) {
     portfolioDisplay.append(portfolioRowEl);
     modalEl.removeClass('reveal');
     modalEl.addclass('hide');
-
 }
+
+function calculateTotalInvestment (price, investment){
+    let total = price * investment;
+    return total;
+}
+
+formEl
