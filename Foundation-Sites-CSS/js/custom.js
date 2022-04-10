23532b276517769527bf261218cb591233e2a91b
Sherreka Burton <rekadarnb@gmail.com>
2022-04-10 16:40:50 -0400

    sticky top nav not working, returned to regular

---
+++ Foundation-Sites-CSS/js/custom.js
@@ -0,0 +1,12 @@
+/* <td id="coin-name">Coin Content Goes Here</td>
+<td id="coin-date">Date Content Goes Here</td>
+<td id="coin-number">Number of Coins Goes Here</td>
+<td id="coin-price">Purchase Price Goes Here</td>
+<td id="coin-investment">Total Investment Goes Here</td> */
+
+let coinNameDisplay = $('#coin-name');
+
+function printCoinInfo (coinName, date, number, price, investment) {
+
+    let portfolioRowElement = $('<tr>');
+}
