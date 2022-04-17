71c40edc9051bfebea109b288a0831892cab11c4
Arashk <arashkyoosha@gmail.com>
2022-04-17 18:18:41 -0400

    code improvements

--- assets/js/convert.js
+++ assets/js/convert.js
@@ -1,12 +1,11 @@
 var mainCon = document.getElementById("container");
 
 const getCryptoData = async () => {
-  fetch("https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,LTC,DASH,DOGE,XRP,SHIB&tsyms=USD"
-  )
+  fetch("https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,LTC,DASH,DOGE,XRP,SHIB&tsyms=USD")
     .then((response) => response.json())
     .then((data) => {
       for (const key in data) {
-        // console.log(key, data[key].USD);
+        console.log(key, data[key].USD);
         var tag = document.createElement("p");
         tag.setAttribute("id", "p");
         tag.innerHTML = `Current ${key} price: $` + data[key].USD;
